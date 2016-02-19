b95c9a187bcba97749fe8f04f6507351b9720180
José Sánchez-Gallego <j.sanchezgallego@uky.edu>
2016-02-19 14:36:12 -0800

    implemented creation of relation DB between new catalogue and manga_target

--- python/mangaSampleDB/utils/catalogue.py
+++ python/mangaSampleDB/utils/catalogue.py
@@ -17,12 +17,21 @@ from __future__ import division
 from __future__ import print_function
 import sqlalchemy as sql
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.orm import mapper, configure_mappers
+from sqlalchemy.ext.declarative import declarative_base
 from SDSSconnect import DatabaseConnection
 from sqlalchemy.engine.reflection import Inspector
 from astropy import table
+import warnings
 import numpy as np
 import os
 import sys
+
+
+def _warning(message, category=UserWarning, filename='', lineno=-1):
+    print('{0}: {1}'.format(category.__name__, message))
+
+warnings.showwarning = _warning
 
 
 def getPSQLtype(numpyType, colShape):
@@ -68,10 +77,150 @@ def _createNewTable(catName, catData, engine):
 
     meta = sql.MetaData(schema='mangasampledb')
     newTable = sql.Table(catName, meta, *columns)
-    newTable.create(engine)
+    meta.create_all(engine)
 
-    return newTable
+    class NewCatTable(object):
+        __table__ = newTable
+        pass
 
+    mapper(NewCatTable, newTable)
+    configure_mappers()
+
+    return NewCatTable
+
+
+def _createCatalogueRecord(db, session, catname, version,
+                           match=None, current=True):
+    """Adds a new row to the mangasampledb.catalogue table."""
+
+    with session.begin():
+        newCatalogue = db.mangasampledb.Catalogue()
+        newCatalogue.catalogue_name = catname
+        newCatalogue.version = version
+        if match:
+            newCatalogue.matched = True
+            newCatalogue.match_description = open(match[1], 'r').read()
+        else:
+            newCatalogue.matched = False
+
+        session.add(newCatalogue)
+
+    # If current is True, checks if other version of this catalogue is
+    # already current. If so, removes it an makes the new one current.
+    if current:
+
+        currentCheck = session.query(
+            db.mangasampledb.CurrentCatalogue.pk).join(
+                db.mangasampledb.Catalogue).filter(
+                    db.mangasampledb.Catalogue.catalogue_name == catname
+        ).scalar()
+
+        with session.begin():
+            if currentCheck:
+                currentToRemove = session.query(
+                    db.mangasampledb.CurrentCatalogue).get(currentCheck)
+                warnings.warn(
+                    'removing {0} {1} as current catalogue'
+                    .format(
+                        currentToRemove.mangasampledb_catalogue.catalogue_name,
+                        currentToRemove.mangasampledb_catalogue.version),
+                    UserWarning)
+                session.delete(currentToRemove)
+
+            newCurrentCatalogue = db.mangasampledb.CurrentCatalogue(
+                catalogue_pk=newCatalogue.pk)
+            session.add(newCurrentCatalogue)
+            warnings.warn('added {0} {1} as current catalogue'
+                          .format(catname, version), UserWarning)
+
+    return newCatalogue.pk
+
+
+def _createRelationalTable(db, session, matchCat, NewCatTable):
+    """Created a relation table between `NewTable` and manga_target."""
+
+    newCatTableName = NewCatTable.__table__.name.lower()
+
+    # Checks if table exists
+    inspector = Inspector.from_engine(db.engine)
+    tables = inspector.get_table_names(schema='mangasampledb')
+
+    newTableMatchCol = [col.lower() for col in matchCat.colnames
+                        if col.lower() != 'mangaid'][0]
+
+    relationalTableName = 'manga_target_to_{0}'.format(newCatTableName)
+
+    if relationalTableName not in tables:
+
+        metadata = sql.MetaData(schema='mangasampledb')
+
+        relationalTable = sql.Table(
+            relationalTableName, metadata,
+            sql.Column('pk', sql.Integer, primary_key=True),
+            sql.Column('manga_target_pk', sql.Integer,
+                       sql.ForeignKey(db.mangasampledb.MangaTarget.pk)),
+            sql.Column('{0}_pk'.format(newCatTableName), sql.Integer,
+                       sql.ForeignKey(NewCatTable.pk)))
+
+        metadata.create_all(db.engine)
+
+        class RelationalTable(object):
+            __table__ = relationalTable
+
+        mapper(RelationalTable, relationalTable)
+
+    else:
+        warnings.warn('table {0} already exists'.format(relationalTableName),
+                      UserWarning)
+
+        decBase = declarative_base(bind=db.engine)
+
+        class RelationalTable(decBase):
+            __tablename__ = relationalTableName
+            __table_args__ = {'autoload': True, 'schema': 'mangasampledb'}
+
+    configure_mappers()
+
+    with session.begin():
+
+        for mangaid, matchValue in matchCat:
+
+            mangaTargetPK = session.query(
+                db.mangasampledb.MangaTarget.pk).filter(
+                    db.mangasampledb.MangaTarget.mangaid == mangaid.strip()
+            ).scalar()
+            if not mangaTargetPK:
+                raise ValueError('ERROR: creating table {0}: '
+                                 'cannot find mangaid={1} in manga_target.'
+                                 .format(relationalTableName, mangaid))
+
+            matchPK = session.query(NewCatTable.pk).filter(
+                sql.text('{0}.{1} = {2}'
+                         .format(newCatTableName,
+                                 newTableMatchCol, matchValue))).scalar()
+            if not matchPK:
+                continue
+                raise ValueError('ERROR: creating table {0}: '
+                                 'cannot find {1}={2} in {3}.'
+                                 .format(relationalTableName,
+                                         newTableMatchCol,
+                                         matchValue,
+                                         newCatTableName))
+
+            # Check if the record already exists
+            matchRecord = session.query(RelationalTable.pk).filter(
+                RelationalTable.manga_target_pk == mangaTargetPK,
+                sql.text('{0}.{1}_pk = {2}'.format(relationalTableName,
+                                                   newCatTableName, matchPK))
+            ).scalar()
+
+            if matchRecord:
+                continue
+            else:
+                newRecord = RelationalTable()
+                newRecord.manga_target_pk = mangaTargetPK
+                setattr(newRecord, '{0}_pk'.format(newCatTableName), matchPK)
+                session.add(newRecord)
 
 # def _updateColumns(catname, catData):
 #     """Checks if new columns need to be added to an existing table."""
@@ -134,7 +283,7 @@ def _checkValue(value):
         return np.where(np.isnan(value), None, value)
 
 
-def ingestCatalogue(catfile, catname, version, current=False,
+def ingestCatalogue(catfile, catname, version, current=True,
                     match=None, connstring=None, step=500, **kwargs):
     """Runs the catalogue ingestion."""
 
@@ -167,21 +316,33 @@ def ingestCatalogue(catfile, catname, version, current=False,
         raise ValueError('table {0} already exists in mangasampledb. '
                          'Drop it before continuing.'.format(catname))
 
-    # Checks if the catalogue name and version already exists. If not, creates
-    # the appropriate catalogue table
+    # Checks if the catalogue name and version already exists. If not, adds
     with session.begin(subtransactions=True):
-        catalogue = session.query(sampleDB.Catalogue).filter(
-            sampleDB.Catalogue.catalogue_name == catname).scalar()
-        if catalogue is not None:
-            if catalogue.version == version:
-                raise RuntimeError('(CATNAME, VERSION) already exist '
-                                   'in mangaSampleDB.')
+        catalogue = session.query(sampleDB.Catalogue.pk).filter(
+            sampleDB.Catalogue.catalogue_name == catname,
+            sampleDB.Catalogue.version == version).scalar()
+
+    if catalogue is not None:
+        catPK = catalogue
+        warnings.warn('(CATNAME, VERSION) already exist in mangaSampleDB.',
+                      UserWarning)
+    else:
+        print('INFO: creating record in mangasampledb.catalogue for '
+              'CATNAME={0}, VERSION={1}.'.format(catname, version))
+        catPK = _createCatalogueRecord(db, session, catname, version,
+                                       match=match, current=current)
 
     # Reads the catalogue file
     catData = table.Table.read(catfile, format='fits')
 
+    # Reads matching file, if any
+    if match:
+        matchCat = table.Table.read(match[0])
+    else:
+        matchCat = None
+
     # Creates the new table.
-    newTable = _createNewTable(catname, catData, db.engine)
+    NewCatTable = _createNewTable(catname, catData, db.engine)
 
     colnames = catData.colnames
     nRows = len(catData)
@@ -222,16 +383,23 @@ def ingestCatalogue(catfile, catname, version, current=False,
                 if not np.isscalar(value) and not np.any(value):
                     value = None
                 dd[col.lower()] = value
+            dd['catalogue_pk'] = catPK
             data.append(dd)
 
-        db.engine.execute(newTable.insert(data))
+        db.engine.execute(NewCatTable.__table__.insert(data))
 
         if mm % step == 0:
             sys.stdout.write(
-                'INFO: inserted {0} rows out of {1}.\r'.format(mm, nRows))
+                '\x1b[2KINFO: inserted {0} rows out of {1}.\r'
+                .format(mm, nRows))
             sys.stdout.flush()
 
         nn = mm
 
-    sys.stdout.write('INFO: inserted {0} rows.'.format(nRows))
+    sys.stdout.write('INFO: inserted {0} rows.\n'.format(nRows))
     sys.stdout.flush()
+
+    # If there is a matching catalogue, we create the table relating
+    # the new catalogue with mangasampledb.manga_target.
+    if matchCat:
+        _createRelationalTable(db, session, matchCat, NewCatTable)
